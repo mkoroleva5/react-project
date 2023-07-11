896afa65a8d7207481c1f1d16e85679484658da2
mkoroleva5 <mkoroleva5@gmail.com>
2023-07-11 15:25:40 +0300

    config: decompose webpack config

---
+++ config/build/buildPlugins.ts
@@ -0,0 +1,16 @@
+import path from "path";
+import webpack from "webpack";
+import HTMLWebpackPlugin from "html-webpack-plugin";
+import { BuildOptions } from "./types/config";
+
+export function buildPlugins({
+  paths,
+}: BuildOptions): webpack.WebpackPluginInstance[] {
+  return [
+    new HTMLWebpackPlugin({
+      title: "React project",
+      template: paths.html,
+    }),
+    new webpack.ProgressPlugin(),
+  ];
+}
