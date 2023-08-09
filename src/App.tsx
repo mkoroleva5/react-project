a02773730792d1ed016fff69793baf8e6a5993e1
mkoroleva5 <mkoroleva5@gmail.com>
2023-08-09 18:57:08 +0300

    feat: add lazy routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
-import { Counter } from "./components/Counter";
-import "./index.scss";
+import { Suspense } from "react";
+import { Route, Routes, Link } from "react-router-dom";
+import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
+import { MainPageAsync } from "./pages/MainPage/MainPage.async";
+import "./styles/index.scss";
 
 export const App = () => {
   return (
     <div>
-      <Counter />
+      <Link to={"/"}>Home</Link>
+      <Link to={"/about"}>About</Link>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path={"/"} element={<MainPageAsync />} />
+          <Route path={"/about"} element={<AboutPageAsync />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 };
