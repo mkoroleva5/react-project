594e26013c08bf5554fc4a5c6c48908410e6ef20
mkoroleva5 <mkoroleva5@gmail.com>
2023-08-09 20:00:22 +0300

    feat: add theme toggler

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-import { Suspense } from "react";
+import { Suspense, useContext, useState } from "react";
 import { Route, Routes, Link } from "react-router-dom";
 import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
 import { MainPageAsync } from "./pages/MainPage/MainPage.async";
+import { useTheme } from "./theme/useTheme";
 import "./styles/index.scss";
 
 export const App = () => {
+  const { theme, toggleTheme } = useTheme();
+
   return (
-    <div>
+    <div className={`app ${theme}`}>
+      <button onClick={toggleTheme}>TOGGLE</button>
       <Link to={"/"}>Home</Link>
       <Link to={"/about"}>About</Link>
       <Suspense fallback={<div>Loading...</div>}>
