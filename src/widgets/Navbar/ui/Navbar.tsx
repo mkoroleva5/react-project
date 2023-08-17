f259ccbc2ffe2bea808e22fa672a84906c3bce22
mkoroleva5 <mkoroleva5@gmail.com>
2023-08-17 20:35:57 +0300

    feat: translate pages

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,24 +1,23 @@
-import { Link } from "react-router-dom";
-import { routeConfig } from "shared/config/routeConfig/routeConfig";
 import { classNames } from "shared/lib/classNames/classNames";
 import styles from "./Navbar.module.scss";
 import { AppLink, AppLinkTheme } from "shared/ui/AppLink/AppLink";
-import { ThemeSwitcher } from "widgets/ThemeSwitcher";
+import { useTranslation } from "react-i18next";
 
 interface NavbarProps {
   className?: string;
 }
 
 export const Navbar = ({ className }: NavbarProps) => {
+  const { t } = useTranslation();
+
   return (
     <div className={classNames(styles.navbar, [className])}>
-      <ThemeSwitcher />
       <div className={styles.links}>
         <AppLink to={"/"} theme={AppLinkTheme.SECONDARY}>
-          Home
+          {t("Home")}
         </AppLink>
         <AppLink to={"/about"} theme={AppLinkTheme.SECONDARY}>
-          About
+          {t("About")}
         </AppLink>
       </div>
     </div>
