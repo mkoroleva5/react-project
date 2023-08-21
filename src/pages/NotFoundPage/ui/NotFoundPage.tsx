4f0016e672ceeee524be5484c8d0e180281038c9
mkoroleva5 <mkoroleva5@gmail.com>
2023-08-21 22:29:24 +0300

    feat: add NotFoundPage

---
+++ src/pages/NotFoundPage/ui/NotFoundPage.tsx
@@ -0,0 +1,17 @@
+import { useTranslation } from 'react-i18next';
+import { classNames } from 'shared/lib/classNames/classNames';
+import styles from './NotFoundPage.module.scss';
+
+interface NotFoundPageProps {
+  className?: string;
+}
+
+export const NotFoundPage = ({ className }: NotFoundPageProps) => {
+  const { t } = useTranslation();
+
+  return (
+    <div className={classNames(styles.notFoundPage, [className])}>
+      {t('Page not found')}
+    </div>
+  );
+};
