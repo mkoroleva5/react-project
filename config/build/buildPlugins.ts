2519e211c08e366199d97a4388875c70f4e12c41
mkoroleva5 <mkoroleva5@gmail.com>
2023-08-22 19:59:54 +0300

    config: add BundleAnalyzerPlugin

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -2,6 +2,7 @@ import webpack from 'webpack';
 import HTMLWebpackPlugin from 'html-webpack-plugin';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 
 import { BuildOptions } from './types/config';
 
@@ -13,13 +14,20 @@ export function buildPlugins({
     new HTMLWebpackPlugin({
       template: paths.html,
     }),
+
     new webpack.ProgressPlugin(),
+
     new MiniCssExtractPlugin({
       filename: 'css/[name].[contenthash:8].css',
       chunkFilename: 'css/[name].[contenthash:8].css',
     }),
+
     new webpack.DefinePlugin({
       __IS_DEV__: JSON.stringify(isDev),
+    }),
+
+    new BundleAnalyzerPlugin({
+      openAnalyzer: false,
     }),
   ];
 
