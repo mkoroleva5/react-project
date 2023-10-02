431752c70f0064fd8011e7ec1e577b2d17b3d300
mkoroleva5 <mkoroleva5@gmail.com>
2023-10-02 18:03:49 +0300

    feat: add stories for pages

---
+++ src/pages/AboutPage/ui/AboutPage.stories.tsx
@@ -0,0 +1,24 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Theme } from 'app/providers/ThemeProvider';
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import AboutPage from './AboutPage';
+
+const meta: Meta<typeof AboutPage> = {
+  title: 'pages/AboutPage',
+  component: AboutPage,
+  parameters: {},
+  tags: ['autodocs'],
+  argTypes: {},
+};
+
+export default meta;
+type Story = StoryObj<typeof AboutPage>;
+
+export const Light: Story = {
+  args: {},
+};
+
+export const Dark: Story = {
+  args: {},
+  decorators: [ThemeDecorator(Theme.DARK)],
+};
---
+++ src/pages/NotFoundPage/ui/NotFoundPage.stories.tsx
@@ -0,0 +1,24 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Theme } from 'app/providers/ThemeProvider';
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import { NotFoundPage } from './NotFoundPage';
+
+const meta: Meta<typeof NotFoundPage> = {
+  title: 'pages/NotFoundPage',
+  component: NotFoundPage,
+  parameters: {},
+  tags: ['autodocs'],
+  argTypes: {},
+};
+
+export default meta;
+type Story = StoryObj<typeof NotFoundPage>;
+
+export const Light: Story = {
+  args: {},
+};
+
+export const Dark: Story = {
+  args: {},
+  decorators: [ThemeDecorator(Theme.DARK)],
+};
